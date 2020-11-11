b47510e236fefc26e0d4be9176b76e0b7138f1f6
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-11-11 11:10:33 -0500

    Changed the database again in the config folder

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'pokemon-db'
+const mongooseBaseName = 'pokemon-database'
 
 // create the mongodb uri for development and test
 const database = {
